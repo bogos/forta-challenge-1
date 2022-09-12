af358c67c4ce1bb3304cddcbf674475acceb845a
bogos <andre.neyra@gmail.com>
2022-09-12 12:21:44 -0500

    refac: add missing provideHandleTransaction wrap function

--- src/agent.ts
+++ src/agent.ts
@@ -1,48 +1,45 @@
-import {
-  Finding,
-  HandleTransaction,
-  TransactionEvent,
-  FindingSeverity,
-  FindingType,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
+import { HandlerInputs } from "./types";
 
 export const DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
-export const FORTA_PROXY_AGENT_REGISTRTY_CORE =
-  "0x61447385B019187daa48e91c55c02AF1F1f3F863";
+export const FORTA_PROXY_AGENT_REGISTRTY_CORE = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 export const CREATE_AGENT_METHOD =
   "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
 
-export const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
-  if (txEvent.from != DEPLOYER_ADDRESS.toLowerCase()) return findings;
+export const handlerInputs: HandlerInputs = {
+  DEPLOYER_ADDRESS,
+  FORTA_PROXY_AGENT_REGISTRTY_CORE,
+  CREATE_AGENT_METHOD,
+};
 
-  const createAgentMethodCalls = txEvent.filterFunction(
-    CREATE_AGENT_METHOD,
-    FORTA_PROXY_AGENT_REGISTRTY_CORE
-  );
-  createAgentMethodCalls.forEach((methodCall) => {
-    const { agentId, chainIds, owner, metadata } = methodCall.args;
-    findings.push(
-      Finding.fromObject({
-        name: "Create Agent",
-        description: `Forta bot deployed by Nethermind`,
-        alertId: "NM-1",
-        severity: FindingSeverity.Info,
-        type: FindingType.Info,
-        metadata: {
-          agentId: agentId.toString(),
-          owner,
-          chainIds: chainIds.toString(),
-          metadata,
-        },
-      })
-    );
-  });
-  return findings;
+export const providerHandleTransaction = (handlerInputs: HandlerInputs): HandleTransaction => {
+  return async (txEvent: TransactionEvent) => {
+    const { DEPLOYER_ADDRESS, FORTA_PROXY_AGENT_REGISTRTY_CORE, CREATE_AGENT_METHOD } = handlerInputs;
+    const findings: Finding[] = [];
+    if (txEvent.from !== DEPLOYER_ADDRESS.toLowerCase()) return findings;
+    const createAgentMethodCalls = txEvent.filterFunction(CREATE_AGENT_METHOD, FORTA_PROXY_AGENT_REGISTRTY_CORE);
+    createAgentMethodCalls.forEach((methodCall) => {
+      const { agentId, chainIds, owner, metadata } = methodCall.args;
+      findings.push(
+        Finding.fromObject({
+          name: "Create Agent",
+          description: `Forta bot deployed by Nethermind`,
+          alertId: "NM-1",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          metadata: {
+            agentId: agentId.toString(),
+            owner,
+            chainIds: chainIds.toString(),
+            metadata,
+          },
+        })
+      );
+    });
+    return findings;
+  };
 };
 
 export default {
-  handleTransaction,
+  handleTransaction: providerHandleTransaction(handlerInputs),
 };
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type HandlerInputs = {
+  DEPLOYER_ADDRESS: string;
+  FORTA_PROXY_AGENT_REGISTRTY_CORE: string;
+  CREATE_AGENT_METHOD: string;
+};
