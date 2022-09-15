0032aa916bf656149fbaec9042b517f9545c403b
bogos <andre.neyra@gmail.com>
2022-09-15 16:18:07 -0500

    refac: fix typos

--- src/agent.ts
+++ src/agent.ts
@@ -2,28 +2,28 @@ import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingT
 import { HandlerInputs } from "./types";
 
 export const DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
-export const FORTA_PROXY_AGENT_REGISTRTY_CORE = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
+export const FORTA_PROXY_AGENT_REGISTRY_CORE = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 export const CREATE_AGENT_METHOD =
   "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
 
 export const handlerInputs: HandlerInputs = {
   DEPLOYER_ADDRESS,
-  FORTA_PROXY_AGENT_REGISTRTY_CORE,
+  FORTA_PROXY_AGENT_REGISTRY_CORE,
   CREATE_AGENT_METHOD,
 };
 
 export const providerHandleTransaction = (handlerInputs: HandlerInputs): HandleTransaction => {
   return async (txEvent: TransactionEvent) => {
-    const { DEPLOYER_ADDRESS, FORTA_PROXY_AGENT_REGISTRTY_CORE, CREATE_AGENT_METHOD } = handlerInputs;
+    const { DEPLOYER_ADDRESS, FORTA_PROXY_AGENT_REGISTRY_CORE, CREATE_AGENT_METHOD } = handlerInputs;
     const findings: Finding[] = [];
     if (txEvent.from !== DEPLOYER_ADDRESS.toLowerCase()) return findings;
-    const createAgentMethodCalls = txEvent.filterFunction(CREATE_AGENT_METHOD, FORTA_PROXY_AGENT_REGISTRTY_CORE);
+    const createAgentMethodCalls = txEvent.filterFunction(CREATE_AGENT_METHOD, FORTA_PROXY_AGENT_REGISTRY_CORE);
     createAgentMethodCalls.forEach((methodCall) => {
       const { agentId, chainIds, owner, metadata } = methodCall.args;
       findings.push(
         Finding.fromObject({
           name: "Create Agent",
-          description: `Forta bot deployed by Nethermind`,
+          description: "Forta bot deployed by Nethermind",
           alertId: "NM-1",
           severity: FindingSeverity.Info,
           type: FindingType.Info,
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 export type HandlerInputs = {
   DEPLOYER_ADDRESS: string;
-  FORTA_PROXY_AGENT_REGISTRTY_CORE: string;
+  FORTA_PROXY_AGENT_REGISTRY_CORE: string;
   CREATE_AGENT_METHOD: string;
 };
