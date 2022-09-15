bb4df0554194f122717e825def35a5c0d8f2a045
bogos <andre.neyra@gmail.com>
2022-09-15 16:17:37 -0500

    test: add missing test

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,20 +1,27 @@
 import { Finding, FindingSeverity, FindingType, HandleTransaction } from "forta-agent";
-import { TestTransactionEvent } from "forta-agent-tools/lib/tests";
+import { TestTransactionEvent, createAddress } from "forta-agent-tools/lib/tests";
 import {
   CREATE_AGENT_METHOD,
   DEPLOYER_ADDRESS,
-  FORTA_PROXY_AGENT_REGISTRTY_CORE,
+  FORTA_PROXY_AGENT_REGISTRY_CORE,
   handlerInputs,
   providerHandleTransaction,
 } from "./agent";
 import { Interface } from "ethers/lib/utils";
 import { BigNumber } from "ethers";
 
-const FAKE_DATA = {
+const FAKE_DATA_0 = {
   agentId: BigNumber.from("1111"),
-  owner: "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8",
+  owner: createAddress("0x"),
   metadata: "QmPkydGrmSK2roUJeNzsdC3e7Yetr7zb7UNdmiXyRUM6i7",
   chainIds: [BigNumber.from("2222")],
+};
+
+const FAKE_DATA_1 = {
+  agentId: BigNumber.from("3333"),
+  owner: createAddress("0x1"),
+  metadata: "QmPkydGrmSK2roUJeNzsdC3e7Yetr7zb7UNdmiXyRUM6i7",
+  chainIds: [BigNumber.from("4444")],
 };
 
 describe("New Forta Agent deployment", () => {
@@ -34,14 +41,14 @@ describe("New Forta Agent deployment", () => {
   it("returns findings if there is a bot deployment", async () => {
     const txEvent = new TestTransactionEvent()
       .setFrom(DEPLOYER_ADDRESS)
-      .setTo(FORTA_PROXY_AGENT_REGISTRTY_CORE)
+      .setTo(FORTA_PROXY_AGENT_REGISTRY_CORE)
       .addTraces({
-        to: FORTA_PROXY_AGENT_REGISTRTY_CORE,
+        to: FORTA_PROXY_AGENT_REGISTRY_CORE,
         input: fortaProxy.encodeFunctionData("createAgent", [
-          FAKE_DATA.agentId,
-          FAKE_DATA.owner,
-          FAKE_DATA.metadata,
-          FAKE_DATA.chainIds,
+          FAKE_DATA_0.agentId,
+          FAKE_DATA_0.owner,
+          FAKE_DATA_0.metadata,
+          FAKE_DATA_0.chainIds,
         ]),
       });
     const findings = await handleTransaction(txEvent);
@@ -53,10 +60,82 @@ describe("New Forta Agent deployment", () => {
         severity: FindingSeverity.Info,
         type: FindingType.Info,
         metadata: {
-          agentId: FAKE_DATA.agentId.toString(),
-          owner: FAKE_DATA.owner,
-          metadata: FAKE_DATA.metadata,
-          chainIds: FAKE_DATA.chainIds.toString(),
+          agentId: FAKE_DATA_0.agentId.toString(),
+          owner: FAKE_DATA_0.owner,
+          metadata: FAKE_DATA_0.metadata,
+          chainIds: FAKE_DATA_0.chainIds.toString(),
+        },
+      }),
+    ]);
+  });
+
+  it("returns no finding for other deployer", async () => {
+    const OTHER_DEPLOYER = createAddress("0x1");
+    const txEvent = new TestTransactionEvent()
+      .setFrom(OTHER_DEPLOYER)
+      .setTo(FORTA_PROXY_AGENT_REGISTRY_CORE)
+      .addTraces({
+        to: FORTA_PROXY_AGENT_REGISTRY_CORE,
+        input: fortaProxy.encodeFunctionData("createAgent", [
+          FAKE_DATA_0.agentId,
+          FAKE_DATA_0.owner,
+          FAKE_DATA_0.metadata,
+          FAKE_DATA_0.chainIds,
+        ]),
+      });
+
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([]);
+  });
+
+  it("multiple findings for multiple deployment by Nethermind", async () => {
+    const txEvent = new TestTransactionEvent()
+      .setFrom(DEPLOYER_ADDRESS)
+      .setTo(FORTA_PROXY_AGENT_REGISTRY_CORE)
+      .addTraces({
+        to: FORTA_PROXY_AGENT_REGISTRY_CORE,
+        input: fortaProxy.encodeFunctionData("createAgent", [
+          FAKE_DATA_0.agentId,
+          FAKE_DATA_0.owner,
+          FAKE_DATA_0.metadata,
+          FAKE_DATA_0.chainIds,
+        ]),
+      })
+      .addTraces({
+        to: FORTA_PROXY_AGENT_REGISTRY_CORE,
+        input: fortaProxy.encodeFunctionData("createAgent", [
+          FAKE_DATA_1.agentId,
+          FAKE_DATA_1.owner,
+          FAKE_DATA_1.metadata,
+          FAKE_DATA_1.chainIds,
+        ]),
+      });
+    const findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "Create Agent",
+        description: `Forta bot deployed by Nethermind`,
+        alertId: "NM-1",
+        severity: FindingSeverity.Info,
+        type: FindingType.Info,
+        metadata: {
+          agentId: FAKE_DATA_0.agentId.toString(),
+          owner: FAKE_DATA_0.owner,
+          metadata: FAKE_DATA_0.metadata,
+          chainIds: FAKE_DATA_0.chainIds.toString(),
+        },
+      }),
+      Finding.fromObject({
+        name: "Create Agent",
+        description: `Forta bot deployed by Nethermind`,
+        alertId: "NM-1",
+        severity: FindingSeverity.Info,
+        type: FindingType.Info,
+        metadata: {
+          agentId: FAKE_DATA_1.agentId.toString(),
+          owner: FAKE_DATA_1.owner,
+          metadata: FAKE_DATA_1.metadata,
+          chainIds: FAKE_DATA_1.chainIds.toString(),
         },
       }),
     ]);
